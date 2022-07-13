de18d20707444c0026e9814bc9e29b55aace878a
MrAbbos <mbotirjonov2002@gmail.com>
2022-07-13 16:08:43 +0500

    implemented get batch function

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,24 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	var mx sync.Mutex
+	var wg sync.WaitGroup
+	var i int64 = 0
+
+	ch := make(chan struct{}, pool)
+	for ; i < n; i++ {
+		wg.Add(1)
+		ch <- struct{}{}
+		go func(i int64) {
+			el := getOne(i)
+			mx.Lock()
+			res = append(res, el)
+			mx.Unlock()
+			<-ch
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
